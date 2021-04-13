e6b6068d958e727d4cf489b98e738941103c1742
Gabriel Rezende <g@brielrezen.de>
2021-04-13 20:19:59 -0300

    primeira rota funcionando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const routes = require("./routes")
 const PORT = 3030;
 
 app.use(cors())
 app.use(express.json()) // Define content-type
+app.use('/api', routes)
 
 app.listen(PORT, () => console.log('server running at ' + PORT))
