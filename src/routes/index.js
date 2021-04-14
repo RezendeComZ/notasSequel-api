7f28840f6ad92ff5183bb8ac35bc0f4d4e7682eb
Gabriel Rezende <g@brielrezen.de>
2021-04-14 16:49:06 -0300

    retrieving users list

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const { Router } = require('express');
+const { Usuario, Nota} = require('../models')
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('Hello World')
+router.get('/users/list', async (req, res) => {
+  try {
+    let result = await Usuario.findAll();
+    return res.status(200).json(result)
+  } catch (error) {
+    return res.status(400).json({ message: 'Error: ' + error.message })
+  }
 })
 
 module.exports = router
