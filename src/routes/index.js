9390e5117dfb7422a7e6a172d39d951a394b7836
Gabriel Rezende <g@brielrezen.de>
2021-04-14 18:16:53 -0300

    post function working

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { Usuario, Nota} = require('../models')
 
 const router = Router();
 
+// Returning a user list
 router.get('/users/list', async (req, res) => {
   try {
     let result = await Usuario.findAll();
@@ -10,6 +11,26 @@ router.get('/users/list', async (req, res) => {
   } catch (error) {
     return res.status(400).json({ message: 'Error: ' + error.message })
   }
+});
+
+// Post:
+router.post('/post', async (req, res) => {
+  try {
+    console.log("chegou no try")
+    let post = await Nota.create({...req.body});
+    return res.status(201).json(post)
+  } catch (error) {
+    console.log("chegou no erro")
+    return res.status(400).json({ message: 'Error: ' + error.message })
+  }
 })
+/*
+Insomnia POST example:
+{
+	"user_id": 1,
+	"title": "Via insomnia",
+	"body": "e o body"
+}
+*/
 
 module.exports = router
