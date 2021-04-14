34867ba9a27dc0ff74f173788931de11c8b8b7e9
Gabriel Rezende <g@brielrezen.de>
2021-04-14 18:25:31 -0300

    /allPostsByUser

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,8 @@ const { Usuario, Nota} = require('../models')
 
 const router = Router();
 
+//GET:
+
 // Returning a user list
 router.get('/users/list', async (req, res) => {
   try {
@@ -13,7 +15,29 @@ router.get('/users/list', async (req, res) => {
   }
 });
 
-// Post:
+router.get('/allPostsByUser', async (req, res) => {
+  try {
+    let { user_id } = req.body
+    let allPostsByUser = await Nota.findAll({
+      where: {
+        user_id
+      }
+    }
+    )
+    return res.status(200).json(allPostsByUser)
+  } catch (error) {
+    return res.status(400).json({ message: 'Error: ' + error.message })
+  }
+})
+/*
+Insomnia GET Example:
+{
+	"user_id": 1
+}
+*/
+
+
+// POST:
 router.post('/post', async (req, res) => {
   try {
     console.log("chegou no try")
